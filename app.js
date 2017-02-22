a4f59dd564118cebe7ea412c6e362a93ed191020
Taylor Hanayik <hanayik@gmail.com>
2017-02-22 09:30:57 -0500

    edit brightness check and volume check

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const appRootDir = require('app-root-dir').get() //get the path of the applicati
 const ffmpeg = appRootDir+'/ffmpeg/ffmpeg'
 const exec = require( 'child_process' ).exec
 const system = require('system-control')();
+const notifier = require('electron-notifications')
+app.setName('PRT')
 //icon credit: http://www.flaticon.com/authors/madebyoliver
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -41,7 +43,7 @@ function createWindow () {
 
   function checkForMaxBrightness() {
     system.display.getBrightness().then(function(brightness) {
-      console.log(brightness)
+      //console.log(brightness)
       if (brightness < 1) {
         dialog.showMessageBox({ type: 'info', buttons: ['Adjust brightness for me', 'Cancel'], message: "Your screen seems a bit dim, would you like to set it to it's maximum setting? It's best to have it as bright as possible." }, function (buttonIndex) {
           if (buttonIndex == 0) {
@@ -49,6 +51,24 @@ function createWindow () {
             system.display.setBrightness(maxBrightness).then(function() {});
           }
         });
+        // USE NOTIFICATIONS WHEN MY PR IS MERGED!
+        //
+        // const notification = notifier.notify(app.getName(), {
+        //   message: "Your screen is dim.",
+        //   buttons: ['Illuminate', 'Cancel']
+        // })
+        // notification.on('buttonClicked', (text) => {
+        //   console.log(text)
+        //   if (text === 'Illuminate') {
+        //     console.log('Illuminate clicked!')
+        //     maxBrightness = 1
+        //     system.display.setBrightness(maxBrightness).then(function() {});
+        //   }
+        //   notification.close()
+        // })
+        // notification.on('clicked', () => {
+        //   notification.close()
+        // })
       }
     });
   }
@@ -57,7 +77,7 @@ function createWindow () {
 
   function checkVolumeLevel() {
     system.audio.getSystemVolume().then(function(volumeLevel) {
-      console.log(volumeLevel)
+      //console.log(volumeLevel)
       if (volumeLevel < 75) {
         dialog.showMessageBox({ type: 'info', buttons: ['Adjust volume for me', 'Cancel'], message: "Your volume is set a little low, would you like to set it a little louder?" }, function (buttonIndex) {
           if (buttonIndex == 0) {
